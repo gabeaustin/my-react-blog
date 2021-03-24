c00fb9c4bdc48525c09374c46cf32a5bc43c2130
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-24 00:54:32 -0500

    added routes to linked pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import MainPage from "./components/MainPage";
+import CreatePost from "./components/CreatePost";
 
 const App = () => {
     return (
         <>
             <div className="navbar">
                 <div className="links">
-                    <a href="/mainpage">Main Page</a>
+                    <a href="/">Main Page</a>
                     <a href="/createpost">Create Post</a>
                 </div>
             </div>
+
+            <Router>
+                {/* <Route path = "/" exact render={(props) => <MainPage />} /> */}
+                <Link to = "/" exact>Main Page</Link>
+                <Link to = "/createpost">Create Post</Link>
+            </Router>
         </>
     );
 };
