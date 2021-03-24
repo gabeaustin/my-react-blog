661901bf4637c5d5f9ffd56977594c77aae29fd3
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-24 11:36:50 -0500

    updated form on createPosts page

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -3,13 +3,24 @@ import "../App.css"
 
 export default function CreatePost() {
     return (
-        <div className="CreatePost">
-            <div className="uploadPost">
-                <input type="text" />
-                <textarea />
-                <input type="text"/>
+            <div className="CreatePost">
+                <div className="uploadPost border m-5 p-3">
+                    <div class="mb-3">
+                        <label class="form-label">Author</label>
+                        <input type="text" class="form-control" placeholder="Who Wrote It Best" />
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Title</label>
+                        <input type="text" class="form-control" placeholder="Your Title" />
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Blog Text</label>
+                        <textarea class="form-control" placeholder="What do you want to blog about?" />
+                    </div>
+                </div>
             </div>
-        </div>
     )
 }
 
