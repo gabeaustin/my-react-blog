7213a02235877854cb89eb8330310757f5ec0b80
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-24 01:16:00 -0500

    started backend portion

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import MainPage from "./components/MainPage";
 import CreatePost from "./components/CreatePost";
+
+const mainPgTitle = () => <h1>Main Page</h1>
+const createPgTitle = () => <h1>Create Page</h1>
 
 const App = () => {
     return (
         <>
-            <div className="navbar">
-                <div className="links">
-                    <a href="/">Main Page</a>
-                    <a href="/createpost">Create Post</a>
-                </div>
-            </div>
-
             <Router>
+                <div className="navbar">
+                    <div className="links">
+                        <Link to = "/" exact>Main Page</Link>
+                        <Link to = {{pathname: "/createpost"}}>Create Post</Link>
+                    </div>
+                </div>
                 {/* <Route path = "/" exact render={(props) => <MainPage />} /> */}
-                <Link to = "/" exact>Main Page</Link>
-                <Link to = "/createpost">Create Post</Link>
             </Router>
         </>
     );
